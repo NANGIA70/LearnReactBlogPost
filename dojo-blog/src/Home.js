14809a98d251e7324cfb394d4d3de47febc974b3
NANGIA70 <adityanangia70@gmail.com>
2025-05-19 15:42:01 -0400

    Removed dojo-blog as sub-repo; added as normal folder

---
+++ dojo-blog/src/Home.js
@@ -0,0 +1,20 @@
+// import { useState, useEffect } from "react";
+
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+    const { data : blogs, isPending, error } = useFetch('http://localhost:8010/blogs');
+
+    return (
+        <div className="Home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs!" handleDelete={handleDelete}/> */}
+            {/* <button onClick = {() => setName('luigi')}>Change Name</button> */}
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ dojo-blog/src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+            <h1>The Dojo</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Block</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
+
+// sfc -> stateless functional component
