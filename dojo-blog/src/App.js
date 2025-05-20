9f1f047662b1231e17c4cf8b21c8ea6ecbd255cb
NANGIA70 <adityanangia70@gmail.com>
2025-05-19 21:42:29 -0400

    Updates

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,15 +1,28 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>  
+            <Route path="/create">
+              <Create />
+            </Route>
+            {/* <Route path="/create" exact component={Home} /> */}
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
